68d431f9c58da53e60030153e0a0901f10b5bae2
hunnellm <137966235+hunnellm@users.noreply.github.com>
2023-09-08 20:53:30 -0400

    Add files via upload

--- helper.py
+++ helper.py
@@ -1,8 +1,22 @@
-def non_adj_twins(g):
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def closed_nbr(g,v):
+    nbrs=[]
+
+    for i in g.neighbor_iterator(v):
+        nbrs.append(i)
+    nbrs.append(v)
+    return set(nbrs)
+
+def adj_twins(g):
     twins=[]
     for v in g.vertices():
         for w in g.vertices():
             if w>v:
-                if g.neighbors(v)==g.neighbors(w): #neighborhoods are open by default
+                if closed_nbr(g,v)==closed_nbr(g,w): 
                         twins.append([v,w])
     return(twins)
