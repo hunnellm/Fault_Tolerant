0258847ce9a4c9952b3a244da622946eaa5218d7
hunnellm <137966235+hunnellm@users.noreply.github.com>
2023-09-15 22:08:56 -0400

    Update fault_tolerant.py

--- fault_tolerant.py
+++ fault_tolerant.py
@@ -20,8 +20,26 @@ def ft_set(g,s, faults =1):
             if sub_s_count==len(sub_s):
                 break
             if len(zerosgame(g,sub_s))==g.order():
-                print sub_s
+                #print sub_s
                 sub_s_count+=1
             else:
                 all_checks=False
     return all_checks
+
+def ftZ(g,faults=1,robust=False,all_sets=False):
+    ftz=-1
+    Z=find_gZ(g)
+    ftz_sets=[]
+    for i in range(faults,g.order()):
+        S=[]
+        for s in subsets(g,Z+i):
+            S.append(s)
+            
+        for j in range(len(S)):
+            if ft_set(g,S[j])==True:
+                ftz=len(S[j])
+                ftz_sets.append(S[j])
+        if all_sets==True:
+            return ftz_sets        
+        else:    
+            return ftz
