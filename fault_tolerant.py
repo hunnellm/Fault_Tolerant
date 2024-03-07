c273248705edb5041de8a7a3c0bd478b39aa4f4f
Mark Hunnell <137966235+hunnellm@users.noreply.github.com>
2024-03-07 14:41:51 -0500

    Update fault_tolerant.py

--- fault_tolerant.py
+++ fault_tolerant.py
@@ -8,38 +8,47 @@ Created on Fri Sep 15 21:33:17 2023
 
 def ft_set(g,s, faults =1):
     
-    Z=find_gZ(g)
+    Z=find_Z(g)
     if Z+faults>g.order():
         return -1
     all_checks=True
+    sub_s_count=0
     if len(zerosgame(g,s))==g.order():
             
+        
+        for sub_s in Subsets(s,len(s)-faults):
             
-        for sub_s in subsets(s,len(s)-faults):
-            sub_s_count=0
-            if sub_s_count==len(sub_s):
-                break
+            #print("sub_s=",sub_s)
+            
             if len(zerosgame(g,sub_s))==g.order():
-                #print sub_s
+                #print("it worked")
                 sub_s_count+=1
-            else:
-                all_checks=False
-    return all_checks
+                #print("subsets=",len(Subsets(s,len(s)-faults)))
+                #print("count=",sub_s_count)
+            if sub_s_count==len(Subsets(s,len(s)-faults)):
+                return True    
+            #else:
+                #return False 
+    return False
 
 def ftZ(g,faults=1,robust=False,all_sets=False):
     ftz=-1
-    Z=find_gZ(g)
+    Z=find_Z(g)
     ftz_sets=[]
     for i in range(faults,g.order()):
+        
         S=[]
-        for s in subsets(g,Z+i):
+        for s in Subsets(g,Z+i):
             S.append(s)
-            
+            #print(S)
         for j in range(len(S)):
             if ft_set(g,S[j])==True:
-                ftz=len(S[j])
-                ftz_sets.append(S[j])
-        if all_sets==True:
-            return ftz_sets        
-        else:    
-            return ftz
+                if ftz==-1 or len(S[j])==ftz:
+                    ftz=len(S[j])
+                    ftz_sets.append(S[j])
+                    print(S[j],"is fault tolerant")     
+                    print(ftz_sets)                      
+    if all_sets==True:
+        return ftz_sets        
+    else:    
+        return ftz
