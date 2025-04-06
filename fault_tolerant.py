d46d1deb8859916ac3c0015a3cbad7de96d78e7f
Mark Hunnell <137966235+hunnellm@users.noreply.github.com>
2025-04-05 21:34:05 -0400

    added prop time functions

--- fault_tolerant.py
+++ fault_tolerant.py
@@ -96,15 +96,19 @@ def ftZ(g,faults=1,robust=False,all_sets=False):
     else:    
         return ftz	    
 
-def psdgame(g,B=[]):
-	#returns the set of vertices colored by the psd color change tule
+def psdgame(g,B=[],prop=False):
 	#g a graph
-	#B the initially blue vertices
+	#blue a set of initially blue vertices
 	again=1
 	filled_vertices=set(B)
 	vertices_to_add=set()
+	num_rounds=0
 	while again==1:
 		again=0
+		if len(filled_vertices)<g.order():
+			num_rounds+=1
+			#print(num_rounds)
+			#print("filled_vertices =",filled_vertices )
 		h=deepcopy(g)
 		filled_vertices=filled_vertices.union(vertices_to_add)
 		for b in filled_vertices:
@@ -118,7 +122,10 @@ def psdgame(g,B=[]):
 				if len(comp_vert)==1:
 					vertices_to_add.add(comp_vert[0])
 					again=1
-	return(filled_vertices)
+	if prop==True and len(filled_vertices)==g.order():
+		return(num_rounds)
+	else:
+		return(filled_vertices)
 
 def Zplus(g):
 	#returns PSD forcing number of graph g
@@ -134,3 +141,113 @@ def Zplus(g):
 		size+=1
 		again=1    
 
+def Z(g):
+	size=1
+	order=len(g.vertices())
+	#print(order)
+	N=set(g.vertices())
+	again=1
+	while again==1:
+		again=0
+		for sub_s in Subsets(N,size):
+			#print(sub_s)
+			#print(len(psdgame(g,sub_s)))
+			if len(gzerosgame(g,sub_s))==order:
+				#print("victory")
+				return size
+		#print("size=",size)
+		size+=1
+		again=1
+def pt_plus(G,S):
+    """
+    pt_plus(G,S) returns the propagation time for
+    positive semidefinite zero forcing on the graph G
+    given a set S
+
+    
+    INPUT:
+            graph G, vertex subset S
+        
+    OUTPUT: 
+            Returns the propagation time based on the
+            positive semidefinite color change rule.
+    
+    EXAMPLES:
+        sage: pt_plus(graphs.CompleteGraph(6),[0,1,2,3])
+        -1
+        sage: pt_plus(graphs.PetersenGraph(),[0,2,8,7]) 
+        2
+    """
+    prop_time = 0
+    H = copy(G)
+    H.delete_vertices(S)
+    T = list(copy(S))
+    while len(H.connected_components())==1:
+        prop_time += 1
+        new_T=copy(T)
+        for v in T:
+            N = set(G.neighbors(v))
+            N.difference_update(set(T))
+            if len(N) == 1:
+                new_T.append(N.pop())
+        if T == new_T:
+            return -1
+        T = new_T
+        H = copy(G)
+        H.delete_vertices(T)
+    if len(H.connected_components())==0:
+        return prop_time
+    else:
+        sub_times=[]
+        for V in H.connected_components():
+            B=set(G.vertices())
+            B.difference_update(set(V))
+            B=list(B)
+            sub_times.append(pt_plus(G,B))
+            if sub_times[-1]==-1:
+                return -1
+        return prop_time+max(sub_times)
+
+def pt(G,S):
+    """
+    pt_plus(G,S) returns the propagation time for
+    positive semidefinite zero forcing on the graph G
+    given a set S
+
+    
+    INPUT:
+            graph G, vertex subset S
+        
+    OUTPUT: 
+            Returns the propagation time based on the
+            positive semidefinite color change rule.
+    
+    EXAMPLES:
+        sage: pt_plus(graphs.CompleteGraph(6),[0,1,2,3])
+        -1
+        sage: pt_plus(graphs.PetersenGraph(),[0,2,8,7]) 
+        2
+    """
+    prop_time = 0
+    H = copy(G)
+    #H.delete_vertices(S)
+    T = list(copy(S))
+    while len(T)<G.order():
+        prop_time += 1
+        new_T=copy(T)
+        #print("t=,",T)
+        for v in T:
+            N = set(G.neighbors(v))
+            M=N.difference(set(T))
+            #print("new_T=",new_T)
+            if len(M) == 1:
+                if list(M)[0] not in new_T:
+                    new_T.append(M.pop())
+                #else:
+                    #print("error")
+        #print(new_T)		
+        if T == new_T:
+            return -1
+        T = new_T
+    if len(T)==G.order():
+        return prop_time
